9a31d1da16436305e998dc4ed31a364386c5984a
庞辉 <huipang@corp.netease.com>
2015-03-03 14:41:21 +0800

    修改读取字符串函数，提高读取速度；

--- CommonJSAPI_Android/src/com/netease/JSBridge/LDJSPluginManager.java
+++ CommonJSAPI_Android/src/com/netease/JSBridge/LDJSPluginManager.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -364,9 +365,9 @@ public class LDJSPluginManager {
 	   * @param strFilePath
 	   * @return
 	   */
-	    private static String readTxtFile(String strFilePath){
+	   private static String readTxtFile(String strFilePath){
 	        String path = strFilePath;
-	        String content = ""; //文件内容字符串
+	        String result = ""; //文件内容字符串
             //打开文件
             File file = new File(path);
             //如果path是传递过来的参数，可以做一个非目录的判断
@@ -376,30 +377,25 @@ public class LDJSPluginManager {
             }
             else
             {
-                try {
-                    InputStream instream = new FileInputStream(file);
-                    if (instream != null)
-                    {
-                        InputStreamReader inputreader = new InputStreamReader(instream);
-                        BufferedReader buffreader = new BufferedReader(inputreader);
-                        String line;
-                        //分行读取
-                        while (( line = buffreader.readLine()) != null) {
-                            content += line + "\n";
-                        }
-                        instream.close();
-                    }
-                }
-                catch (java.io.FileNotFoundException e)
-                {
-                    Log.d("TestFile", "The File doesn't not exist.");
-                }
-                catch (IOException e)
-                {
-                     Log.d("TestFile", e.getMessage());
-                }
+            		long length = file.length();
+            		try{
+            			FileReader in = new FileReader(file);
+            		    char[] contents = new char[(int)length];
+            		    int numRead = in.read(contents);
+            		    if (numRead != length)
+            		    {
+            		    		Log.e("TestFile", "Incomplete read of " + strFilePath +
+            		                 ". Read chars " + numRead + " of " + length);
+            		    }
+            		    result = new String(contents, 0, numRead);
+            		    in.close();
+            		  }
+            		  catch (Exception ex){
+            			  Log.e("TestFile", "Failure reading " + strFilePath, ex);
+            			  result = "";
+            		  }
             }
-            return content;
+            return result;
 
 	    }
 
